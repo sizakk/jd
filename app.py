d112a16dacb6ea4a8115962820bd514fe93be8d4
sizak <44841310+sizakk@users.noreply.github.com>
2024-11-03 22:51:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from main_screen import show_main_screen
 from previous_version import show_previous_version
 
 # 사이드바 메뉴 생성
-st.sidebar.title("화면 선택")
-page = st.sidebar.radio("페이지를 선택하세요:", ("현재 화면", "이전 버전"))
+st.sidebar.title("버전 선택")
+page = st.sidebar.radio("페이지를 선택하세요:", ("현재 버전", "이전 버전"))
 
 # 선택된 페이지에 따라 다른 화면 표시
 if page == "현재 화면":
